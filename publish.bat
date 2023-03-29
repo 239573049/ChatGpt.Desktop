661830b105055e6926b7cc8004310934c7e23161
token <239573049@qq.com>
2023-03-30 02:43:07 +0800

    修改logo

--- publish.bat
+++ publish.bat
@@ -1,3 +1,7 @@
 dotnet publish "./src/ChatGpt.Desktop/ChatGpt.Desktop.csproj" -p:PublishSingleFile=true -c Release --os linux -o ./linux
 dotnet publish "./src/ChatGpt.Desktop/ChatGpt.Desktop.csproj" -p:PublishSingleFile=true -c Release --os win -o ./win
 dotnet publish "./src/ChatGpt.Desktop/ChatGpt.Desktop.csproj" -p:PublishSingleFile=true -c Release --os osx -o ./osx
+
+docker compose build
+
+docker compose push 
