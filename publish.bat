a7beaaf21e7010048d7d11e10cdfed84ff1f68c9
token <239573049@qq.com>
2023-04-07 10:41:26 +0800

    增加复制提示

--- publish.bat
+++ publish.bat
@@ -1,7 +1,9 @@
 dotnet publish "./src/ChatGpt.Desktop/ChatGpt.Desktop.csproj" -p:PublishSingleFile=true -c Release --os linux -o ./linux
 dotnet publish "./src/ChatGpt.Desktop/ChatGpt.Desktop.csproj" -p:PublishSingleFile=true -c Release --os win -o ./win
 dotnet publish "./src/ChatGpt.Desktop/ChatGpt.Desktop.csproj" -p:PublishSingleFile=true -c Release --os osx -o ./osx
-
+dotnet publish "./src/ChatGpt.Desktop/ChatGpt.Desktop.csproj" -p:PublishSingleFile=true --self-contained -c Release --os linux -o ./linux-self-contained
+dotnet publish "./src/ChatGpt.Desktop/ChatGpt.Desktop.csproj" -p:PublishSingleFile=true --self-contained  -c Release --os win -o ./win-self-contained
+dotnet publish "./src/ChatGpt.Desktop/ChatGpt.Desktop.csproj" -p:PublishSingleFile=true --self-contained  -c Release --os osx -o ./osx-self-contained
 docker compose build
 
 docker compose push 
